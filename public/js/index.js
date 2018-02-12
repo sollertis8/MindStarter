5990acd8683542b6d6a7a08aff59c956c7b9a994
sollertis8 <mizumedia@gmail.com>
2018-02-12 15:43:04 -0500

    added functionality for finding relationships with nodes up to 4 levels deep

--- public/js/index.js
+++ public/js/index.js
@@ -1,36 +1,19 @@
 'use strict';
 $(document).ready(function () {
     watchSubmit();
-
+    watchUpdate();
+    $('.js-update-project').hide();
+    $('.js-project-title').hide();
 });
 
-window.FontAwesomeConfig = { searchPseudoElements: true };
-
-// const {Project} = require('./models');
-// const Project = require('./router');
-// const {router} = require('./router');
-// const router = express.Router();
-
-// const Project = require('../routes/index.js')
-
-
-// module.exports = {Project, router};
-
-
-// let parent = `{    "name" : "${idea_word}",
-//                    "children" : "${children}", 
-//                    "relationship_type" : "${relationship_type}",
-//                    "depth" : "${depth}"
-//                  }`;
-
-// let child = `{
-//     "idea_word" : "${idea_word}",
-//     "relationship_type" : "${relationship_type}",
-//     "depth" : "${depth}"
-//   }`;
-
-// router.use('/project', Project)
-
+window.FontAwesomeConfig = {
+    searchPseudoElements: true
+};
+let idea_word = "";
+let projectJson = {
+    "name": idea_word,
+    "children": []
+};
 
 // gets relationship data from the api
 function getDataFromApi(relationship, word, depth, callback) {
@@ -111,73 +94,16 @@ function formDataToJson(project_name, idea_word, relationship_type, depth, callb
     };
     $.ajax(settings)
 }
-// will fix using global variable later
-function displayResponseData(response) {
-    if(response != 0){
 
-     const ellipsis = '\uf142';
-    const projectJson = {
 
-        "name": idea_word,
-        "children": []
-
-    };
-    // const results = JSON.stringify(response);
-    
-    for (var i = 0; i < response.length; i++) {
-        projectJson.children.push({
-            name: response[i].word,
-            icon: 'hello',
-            size: "2000"
-        });
-    }
-projectJson.children.splice(0, 0, {
-        name: "awesome",
-        children: [
-            {name: "amazing", size: "500"},
-            {name: "spectacular", size: "500"},
-            {name: "unbelievable", size: "500"},
-            {name: "fantastic", size: "500"}
-        ]
-    });
-    // const fs = require('fs');
-
-    // fs.exists('project.json', (exists) => {
-    //     if (exists) {
-    //         console.log("file exists");
-    //         fs.readFile('project.json', function readFileCallBack(err, data) {
-    //             if (err) {
-    //                 console.log(err);
-    //             } else {
-    //                 obj = JSON.parse(data);
-    //                 for (i = 0; i < data.length; i++) {
-    //                     obj.project.push(projectJson);
-    //                 }
-    //                 const json = JSON.stringify(obj);
-    //                 fs.writeFile('project.json', json);
-
-    //             }
-    //         });
-    //     } else {
-    //         console.log("file does not exist")
-    //         for (i = 0; i < data.length; i++) {
-    //             obj.project.push(projectJson);
-    //         }
-    //         const json = JSON.stringify(obj);
-    //         fs.writeFile('project.json', json);
-    //     }
-    // })
-    // var margin = {top: 20, right: 10, bottom: 20, left: 10};
-    // var width = 800 - margin.left - margin.right;
-    // var height = 480 - margin.top - margin.bottom;
-   
+function renderProject() {
     var width = 960;
     var height = 960;
 
     var svg = d3.select("svg"),
         margin = 20,
-        diameter = +svg.attr("width"),
-        g = svg.append("g").attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+        diameter = +svg.attr("width");
+        window.g = svg.append("g").attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
 
     var color = d3.scaleLinear()
         .domain([-1, 5])
@@ -188,8 +114,6 @@ projectJson.children.splice(0, 0, {
         .size([diameter - margin, diameter - margin])
         .padding(2);
 
-    // d3.json("project.json", function (error, root) {
-    //     if (error) throw error;
     var root = projectJson;
     root = d3.hierarchy(root)
         .sum(function (d) {
@@ -203,57 +127,49 @@ projectJson.children.splice(0, 0, {
         nodes = pack(root).descendants(),
         view;
 
-    // var ellipsis = $('.node--leaf').html(`<i class="fa fa-cog fa-fw"></i>`)
-
     var rectangle = svg.append('rect')
-    .attr({
-        'width': width * 0.8,
-        'height': height * 0.8,
-        'x': width * 0.1,
-        'y': height * 0.1,
-        'fill': '#F8F8F8'
-    });
-var foWidth = 100;
-var foHeight = 100;
-var anchor = {'w': width/3, 'h': height/3};
-var t = 50, k = 15;
-var tip = {'w': (3/4 * t), 'h': k};
+        .attr({
+            'width': width * 0.8,
+            'height': height * 0.8,
+            'x': width * 0.1,
+            'y': height * 0.1,
+            'fill': '#F8F8F8'
+        });
+    var foWidth = 100;
+    var foHeight = 100;
+    var anchor = {
+        'w': width / 3,
+        'h': height / 3
+    };
+    var t = 50,
+        k = 15;
+    var tip = {
+        'w': (3 / 4 * t),
+        'h': k
+    };
 
-
-    var circle = g.selectAll("circle")
+    var circle = window.g.selectAll("circle")
         .data(nodes)
         .enter().append("circle")
         .attr("class", function (d) {
             return d.parent ? d.children ? "node" : "node node--leaf js-leaf" : "node node--root js-root";
         })
-        .attr("id", (function(d){var a = 0; return function(){return a++}})())
-        
+        .attr("id", (function (d) {
+            var a = 0;
+            return function () {
+                return a++
+            }
+        })())
+
         .style("fill", function (d) {
             return d.children ? color(d.depth) : null;
         })
         .on("click", function (d) {
+
             if (focus !== d) zoom(d), d3.event.stopPropagation();
+            $('.js-idea').val(this.__data__.data.name);
         })
-
-        .on('mouseover', function() {
-            // Selection.html('&#xf040')
-            g.select("circle").selectAll("text")
-            .html('\uf142');
-
-            // .text(function () {
-            //     let icon = selection.html('&#xf040')
-            //     return icon;
-            // .text('&#xf040')
-            // });
-
-        })
-
-        // .on('mouseout', function() {
-        //     g.selectAll('text')
-        //     .remove();
-        // });
-
-    var text = g.selectAll("text")
+    var text = window.g.selectAll("text")
         .data(nodes)
         .enter().append("text")
         .attr("class", "label")
@@ -266,25 +182,7 @@ var tip = {'w': (3/4 * t), 'h': k};
         .text(function (d) {
             return d.data.name;
         })
-        // .text(function (d) {
-        //     return '&#xf040';
-        // // .text('&#xf040')
-        // });
-//  var icon_ellipsis = g.selectAll('text')
-//         .data(nodes)
-//         .enter().append("text")
-//         .attr("class", "ellipsis")
-//         .style("fill-opacity", function() {
-//                 return d3.parent === root ? "block" : "none";
-//         })
-//         .style("display", function (d) {
-//             return d.parent === root ? "block" : "none";
-//         })
-//         .text(function(d){
-//             return d.data.icon;
-//         });
-
-    var node = g.selectAll("circle,text");
+    var node = window.g.selectAll("circle,text");
 
     svg
         .style("background", color(-1))
@@ -299,9 +197,7 @@ var tip = {'w': (3/4 * t), 'h': k};
         focus = d;
 
         var transition = d3.transition()
-        // .attr("class", function (d) {
-        //     return d.children ? "i" :"fas fa-ellipsis-v";
-        // })
+
             .duration(d3.event.altKey ? 7500 : 750)
             .tween("zoom", function (d) {
                 var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
@@ -309,7 +205,7 @@ var tip = {'w': (3/4 * t), 'h': k};
                     zoomTo(i(t));
                 };
             });
-            
+
         transition.selectAll("text")
             .filter(function (d) {
                 return d.parent === focus || this.style.display === "inline";
@@ -318,15 +214,14 @@ var tip = {'w': (3/4 * t), 'h': k};
                 return d.parent === focus || this.style.display === "inline";
             })
             .style("fill-opacity", function (d) {
-                return d.parent === focus ? 1 : 1;
+                return d.parent === focus ? 1 : 0;
             })
             .on("start", function (d) {
                 if (d.parent === focus) this.style.display = "inline";
-                
+
             })
             .on("end", function (d) {
-                // if (d.parent !== focus) this.style.display = "none";
-                if (d.children === focus) this.style.diaplay = "inline";
+                if (d.parent !== focus) this.style.display = "none";
             });
     }
 
@@ -341,24 +236,95 @@ var tip = {'w': (3/4 * t), 'h': k};
         });
     }
     $('.js-project-response').html(root);
-} else {
-    const no_results = "Sorry, there were no results for this combination.  Try a different relationship type.";
-    $('.js-project-response').html(no_results);
 }
 
+
+function clearCanvas() {
+    window.g.remove();
 }
 
-// function watchLoginSubmit() {
-//     $('.js-loginSubmit').submit(event=> {
 
-//     })
-// }
+function displayResponseData(response) {
+    if (response != 0) {
+        for (var i = 0; i < response.length; i++) {
+            projectJson.children.push({
+                name: response[i].word,
+                size: "2000"
+            });
+        }
+        renderProject();
+    } else {
+        const no_results = "Sorry, there were no results for this combination.  Try a different relationship type.";
+        $('.js-project-response').html(no_results);
+    }
 
-let idea_word = "";
+}
+
+
+function displayNodeUpdate(response) {
+    // check that the api response is not empty
+    if (response != "") {
+        // clear the canvas for redrawing
+        clearCanvas();
+        const new_child = [];
+        // checking for a match of child nodes with the selected word
+        for (var i = 0; i < projectJson.children.length; i++) {
+            if (idea_word === projectJson.children[i].name) {
+                for (var k = 0; k < response.length; k++) {
+                    new_child.push({
+                        name: response[k].word,
+                        size: "500"
+                    });
+                }
+                // add new children to specific child node
+                projectJson.children[i].children = new_child;
+                renderProject();
+                // checking if child nodes have any children (2 levels deep)
+            } else if (typeof projectJson.children[i].children != "undefined") {
+                // matching selected word with its node 
+                for (var l = 0; l < projectJson.children[i].children.length; l++) {
+            if (idea_word === projectJson.children[i].children[l].name) {
+                for (var m = 0; m < response.length; m++) {
+                    new_child.push({
+                        name: response[m].word,
+                        size: "250"
+                    });
+                }
+                // adding new children to specific node (now 3 levels deep)
+                projectJson.children[i].children[l].children = new_child;
+                renderProject();
+                // checking if children of child nodes have any children (at 3 levels deep)
+            } else if (typeof projectJson.children[i].children[l].children != "undefined") {
+                // matching selected word with its node
+                for (var n = 0; n < projectJson.children[i].children[l].children.length; n++) {
+            if (idea_word === projectJson.children[i].children[l].children[n].name) {
+                for (var o = 0; o < response.length; o++) {
+                    new_child.push({
+                        name: response[o].word,
+                        size: "125"
+                    });
+                }
+                // adding new children to node now 4 levels deep
+                projectJson.children[i].children[l].children[n].children = new_child;
+                renderProject();
+            } 
+        };
+            }
+        };
+            }
+        };
+    } else if (response == "") {
+        const no_results = "Sorry, there were no results for this combination.  Try a different relationship type.";
+        $('.js-project-response').html(no_results);
+    } 
+        
+    
+};
 
 function watchSubmit() {
     $('.js-project-form').submit(event => {
-        event.preventDefault();
+        console.log('save button clicked');
+        // event.preventDefault();
         const project_name_target = $(event.currentTarget).find('.js-project');
         const idea_target = $(event.currentTarget).find('.js-idea');
         const relationship_target = $(event.currentTarget).find('.js-relationship');
@@ -369,27 +335,29 @@ function watchSubmit() {
         const depth = depth_target.val();
         //  $('.result').text(JSON.stringify($('form').serializeObject()));
         getDataFromApi(relationship_type, idea_word, depth, displayResponseData);
+        console.log('save submitted');
+        $('.js-project-form').hide();
+        $('.js-update-project').show();
+        $('.js-project-title').html(project_name);
+        $('.js-project-title').show();
     });
 }
 
-// function watchDoubleClick(){
-//     $( ".node node--leaf" ).dblclick(event => {
-//         // event.preventDefault();
-//         const project_name_target = $(event.currentTarget).find('.js-project');
-//         const idea_target = $(event.currentTarget).find('.js-idea');
-//         const relationship_target = $(event.currentTarget).find('.js-relationship');
-//         const depth_target = $(event.currentTarget).find('.js-depth');
-//         const project_name = project_name_target.val();
-//         idea_word = idea_target.val();
-//         const relationship_type = relationship_target.val();
-//         const depth = depth_target.val();
-//         //  $('.result').text(JSON.stringify($('form').serializeObject()));
-//         getDataFromApi(relationship_type, idea_word, depth, displayNodeUpdate);
-//         console.log( "Handler for .dblclick() called." );
-//       });
-      
-// }
-
-// function displayNodeUpdate(){
-    
-// }
+function watchUpdate() {
+    $('#update').click(event => {
+        console.log('update button clicked');
+        $('.js-update-project').submit(event => {
+            // event.preventDefault();
+            const project_name_target = $(event.currentTarget).find('.js-project');
+            const idea_target = $(event.currentTarget).find('.js-idea');
+            const relationship_target = $(event.currentTarget).find('.js-relationship');
+            const depth_target = $(event.currentTarget).find('.js-depth');
+            const project_name = project_name_target.val();
+            idea_word = idea_target.val();
+            const relationship_type = relationship_target.val();
+            const depth = depth_target.val();
+            getDataFromApi(relationship_type, idea_word, depth, displayNodeUpdate);
+            console.log('update submitted');
+        });
+    });
+};
